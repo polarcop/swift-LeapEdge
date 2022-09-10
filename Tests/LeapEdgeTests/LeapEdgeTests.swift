6d3ef9108115570326506699ad932e87107bbfbe
Rishi S <rishi8094@users.noreply.github.com>
2022-09-10 15:10:15 +0100

    fix: update tests for new handler

--- Tests/LeapEdgeTests/LeapEdgeTests.swift
+++ Tests/LeapEdgeTests/LeapEdgeTests.swift
@@ -4,16 +4,16 @@ import XCTest
 final class LeapEdgeTests: XCTestCase {
     func testConnection() throws {
         var options: Partial<LeapEdge.InitOptions> = .init()
-//        options[\.debug] = true
+        options[\.debug] = false
         let leap = LeapEdge(auth: .init(token: nil, projectId: "test"), opts: options)
         leap.connect()
         let expectation = expectation(description: "Will connect and receive a message")
         
-        leap.emitter.when { (message: LeapEdge.ConnectionState) in
+        leap.on { (message: LeapEdge.ConnectionState) in
             print("Connection state:", message)
         }
         
-        leap.emitter.when { (message: LeapEdge.ServiceEvent) in
+        leap.on { (message: LeapEdge.ServiceEvent) in
             print("payload", message)
             expectation.fulfill()
         }

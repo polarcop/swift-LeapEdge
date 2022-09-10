7c04b34579822db96cf02d6191d0e7ba2cc9130b
Rishi S <rishi8094@users.noreply.github.com>
2022-09-10 14:43:49 +0100

    feat: leap edge tests

---
+++ Tests/LeapEdgeTests/LeapEdgeTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import LeapEdge
+
+final class LeapEdgeTests: XCTestCase {
+    func testConnection() throws {
+        var options: Partial<LeapEdge.InitOptions> = .init()
+//        options[\.debug] = true
+        let leap = LeapEdge(auth: .init(token: nil, projectId: "test"), opts: options)
+        leap.connect()
+        let expectation = expectation(description: "hello")
+        
+        leap.emitter.when { (message: LeapEdge.ConnectionState) in
+            print("Connection state:", message)
+        }
+        
+        leap.emitter.when { (message: LeapEdge.ServiceEvent) in
+            print("payload", message)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10)
+    }
+}
